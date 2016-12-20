6ddbfdbeb24fa83ce4ffb2d31beb04ea665fae26
Raphael Nestler <raphael.nestler@gmail.com>
2016-12-20 19:34:36 +0100

    20: Reduce blacklist
    Reduce the blacklist to remove overlapping IP ranges.

--- 20/src/lib.rs
+++ 20/src/lib.rs
@@ -58,20 +58,45 @@ impl FromStr for Blacklist {
                 .expect(&format!("Failed to parse line {}: {}", line_number, line))
         }).collect();
         list.sort();
+
+        // reduce list
+        let list = Blacklist::reduce_list(list);
+
         Ok(Blacklist{list: list})
     }
 }
 
 impl Blacklist {
+
+    fn reduce_list(mut list: Vec<IpRange>) -> Vec<IpRange> {
+        let mut i = 0;
+        while i < list.len()-1 {
+            // next list is completly included
+            if list[i+1].start >= list[i].start && list[i+1].end <= list[i].end {
+                list.remove(i+1);
+            // next list is overlapping
+            } else if list[i+1].start <= list[i].end+1 {
+                list[i].end = list[i+1].end;
+                list.remove(i+1);
+            } else {
+                i += 1;
+            }
+        }
+        list
+    }
+
     pub fn is_blocked(&self, ip: u32) -> bool {
         self.list.iter().any(|v| v.is_ip_in_range(ip))
     }
 
     pub fn number_of_allowed_ips(&self) -> usize {
-        (0..4294967295).filter(|v| !self.is_blocked(*v)).count()
+        let number_of_filtered_ips = self.list.iter()
+            .fold(0, |acc, ref x| acc + (x.end - x.start + 1));
+        4294967296 - (number_of_filtered_ips as usize)
     }
 
     pub fn lowest_ip(&self) -> u32 {
+
         let lowest = (0..4294967295).filter(|v| !self.is_blocked(*v)).next();
         lowest.expect("no unblocked IP found")
     }
@@ -94,8 +119,17 @@ mod test {
         let blacklist: Blacklist = input.parse().unwrap();
         assert_eq!(blacklist, Blacklist{list: vec![
             IpRange{start: 0, end: 2},
-            IpRange{start: 4, end: 7},
-            IpRange{start: 5, end: 8},
+            IpRange{start: 4, end: 8},
+        ]});
+    }
+
+    #[test]
+    fn parse_identical_and_overlapping() {
+        let input = "5-8\n0-2\n0-2\n4-5\n6-7\n4-7";
+        let blacklist: Blacklist = input.parse().unwrap();
+        assert_eq!(blacklist, Blacklist{list: vec![
+            IpRange{start: 0, end: 2},
+            IpRange{start: 4, end: 8},
         ]});
     }
 
@@ -105,5 +139,13 @@ mod test {
         let blacklist: Blacklist = input.parse().unwrap();
         let lowest = blacklist.lowest_ip();
         assert_eq!(lowest, 3);
+    }
+
+    #[test]
+    fn test_number_of_allowed_ips() {
+        let input = "5-8\n0-2\n4-7";
+        let blacklist: Blacklist = input.parse().unwrap();
+        let number_of_allowed_ips = blacklist.number_of_allowed_ips();
+        assert_eq!(number_of_allowed_ips, 4294967296 - 8);
     }
 }
